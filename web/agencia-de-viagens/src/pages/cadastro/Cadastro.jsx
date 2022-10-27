6aa51dcea77b3376b8031431684ba6b68f70fa13
José Lucas <bezerra00714@hotmail.com>
2022-10-26 22:21:58 -0300

    problema do post resolvido!

--- web/agencia-de-viagens/src/pages/cadastro/Cadastro.jsx
+++ web/agencia-de-viagens/src/pages/cadastro/Cadastro.jsx
@@ -7,22 +7,25 @@ export default function cadastro() {
 
     let navigate =useNavigate();
 
+    // variaveis para resolver o problema do post
+    const [idDest, setIdDest]= useState();
+    const [idVoo, setIdVoo] = useState();
+
     const [clientes, setCliente] = useState({
         cpf: "",
         origem: "",
         dataIda: "",
-        dataVolta: "",
-        destino:{
-            id: ""
-        },
-        voo:{
-            id:""
-        }
+        dataVolta: ""
+        // destino:{
+        //     id: idDest
+        // },
+        // voo:{
+        //     id: idVoo
+        // }
     })
     const { cpf, origem, dataIda, dataVolta } = clientes;
-    const { id: codDest } = clientes.destino;
-    const {id: codVoo} = clientes.voo;
-
+    // const { id: codDest } = clientes.destino;
+    // const {id: codVoo} = clientes.voo;
 
 
     const [destinos, setDestinos] = useState([]);
@@ -30,6 +33,19 @@ export default function cadastro() {
 
     const onInputChange = (e) =>{
         setCliente({...clientes, [e.target.name]:e.target.value})
+    }
+
+    // para tentar ajustar o problema do post
+    const onInputChangeSelectDest = (e) =>{
+        setIdDest(parseInt(e.target.value));
+        console.log(typeof idDest)
+        
+    }
+    
+    const onInputChangeSelectVoo = (e) =>{
+        setIdVoo(parseInt(e.target.value));
+        console.log(typeof idVoo)
+        
     }
 
 
@@ -42,13 +58,13 @@ export default function cadastro() {
     const carregarDestinos = async () =>{
         const result= await axios.get("http://localhost:8080/api/destinos");
         setDestinos(result.data);
-        console.log(result.data);
+        // console.log(result.data);
     }
 
     const carregarVoo = async () =>{
         const result = await axios.get("http://localhost:8080/api/voos");
         setVoos(result.data);
-        console.log(result.data);
+        // console.log(result.data);
     }
 
 
@@ -57,8 +73,20 @@ export default function cadastro() {
         e.preventDefault();
 
         //não consegui fazer o post funcionar, desculpe.
+        // atualização. consegui, mas não do jeito que eu queria
 
-         await axios.post("http://localhost:8080/api/cliente", clientes)
+         await axios.post("http://localhost:8080/api/cliente", {
+            cpf: cpf,
+            origem: origem,
+            dataIda: dataIda,
+            dataVolta: dataVolta,
+            destino:{
+                id: idDest
+            },
+            voo:{
+                id: idVoo
+            }
+         })
         
         //nessa caso, a pagina inicial
         navigate("/")
@@ -123,11 +151,13 @@ export default function cadastro() {
         <select 
         name="destino" 
         id="destino"
-        onChange={(e)=> onInputChange(e)}
-        value={codDest}>
+        onChange={(e)=> onInputChangeSelectDest(e)}
+        value={idDest}>
+
             {destinos.map((destinos, index)=>(
                 <option key={index} value={destinos.id}>{destinos.id} : {destinos.cidade} - {destinos.obraR}</option>
             ))}
+
         </select>
 	<table className="table">
   		<thead className="thead-dark th-color">
@@ -143,16 +173,10 @@ export default function cadastro() {
                 destinos.map((destinos,index) =>(
                     
                     <tr>
-                        <th scope="row" key={index}>{destinos.id}</th>
+                        <th scope="row" key={destinos.id}>{destinos.id}</th>
                         <td>{destinos.paises}</td>
                         <td>{destinos.cidade}</td>
                         <td>{destinos.obraR}</td>
-                        
-                        {/* <td> 
-                        <Link classNameName='btn btn-primary mx-2' to={`/viewuser/${user.id}`}>View</Link>
-                        <Link classNameName='btn btn-outline-primary mx-2' to={`/editUser/${user.id}`}>Edit</Link>
-                        <button classNameName='btn btn-danger mx-2' onClick={()=> deleteUser(user.id)}>Delete</button>
-                        </td> */}
                     </tr>
                 ))
             }
@@ -166,11 +190,13 @@ export default function cadastro() {
     <select 
         name="voo" 
         id="voo"
-        onChange={(e)=> onInputChange(e)}
-        value={codVoo}>
+        onChange={(e)=> onInputChangeSelectVoo(e)}
+        value={idVoo}>
+
             {voos.map((voo, index)=>(
                 <option key={index} value={voo.id}>{voo.id} : {voo.companhiaA} - {voo.preco},00</option>
             ))}
+
     </select>
 	<table className="table">
  		 <thead className="thead cabecalho-table th-color">
@@ -184,7 +210,7 @@ export default function cadastro() {
           {
                 voos.map((voos,index) =>(
                     <tr>
-                        <th scope="row" key={index}>{voos.id}</th>
+                        <th scope="row" key={voos.id}>{voos.id}</th>
                         <td>{voos.companhiaA}</td>
                         <td>{voos.preco},00</td>
                     </tr>
